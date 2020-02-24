31151b1492251f2d33fda96b424c4b1ccffec6bf
IlyaShihutin <plague.plague@mail.ru>
2020-02-24 16:08:12 +0300

    feat : add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
+let one = [  "","one ", "two ", "three ", "four ", 
+                 "five ", "six ", "seven ", "eight ", 
+                 "nine ","ten ", "eleven ", "twelve ", 
+                 "thirteen ", "fourteen ", "fifteen ", 
+                 "sixteen ", "seventeen ", "eighteen ", 
+                 "nineteen "
+    ];         
+    let ten = [ "","","twenty", "thirty", "forty", 
+                 "fifty", "sixty", "seventy", "eighty", 
+                 "ninety"];
 module.exports = function toReadable (number) {
-  
+number = String(number).split('');
+    let str=''; 
+    if (number[0]==0) return "zero";
+    if (number.length==3) 
+        { 
+            str+=one[number[0]]+'hundred' + ' ';
+            number.shift();
+        }
+    if (number.length==2 && number[0]>=2 )
+        { 
+            str+=ten[number[0]]+' ';
+            number.shift(); 
+        }
+    else if (number.length==2)
+        { 
+            var g=number[0]+number[1];
+            str+=one[Number(g)];
+            return str.trim(); 
+        }
+    if (number[0]!=0)str+=one[number[0]];
+    return str.trim();  
 }
